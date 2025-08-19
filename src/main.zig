4824236b0758795a4402670a81ff0f170cd145b4
Dustin Sallings <dustin@spy.net>
2025-08-18 15:39:39 -1000

    Handling a few of the parse cases

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,52 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 
-pub fn main() !void {}
+pub fn findFiles(alloc: std.mem.Allocator, dir: *std.fs.Dir) !void {
+    var walker = try dir.walk(alloc);
+    defer walker.deinit();
+
+    while (try walker.next()) |entry| {
+        if (entry.kind != .file) {
+            continue;
+        }
+        // src/main.zig:13:28: error: no field or member function named 'ends_with' in '[:0]const u8'
+
+        if (!std.mem.eql(u8, std.fs.path.extension(entry.path), ".on1")) {
+            continue;
+        }
+
+        std.debug.print("opening file: {s}\n", .{entry.path});
+
+        var j = on1.parseFile(alloc, dir, entry.path);
+        if (j) |*e| {
+            defer e.deinit(alloc);
+            std.debug.print("Parsed file: {s}: {s} ({d})\n", .{ entry.path, e.files, e.year });
+        } else |err| {
+            std.debug.print("Failed to parse file: {s}: {}\n", .{ entry.path, err });
+        }
+    }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        _ = gpa.deinit();
+    }
+    const allocator = gpa.allocator();
+
+    var args = std.process.argsAlloc(allocator) catch return;
+    defer std.process.argsFree(allocator, args);
+
+    for (args[1..]) |arg| {
+        std.debug.print("Opening dir {s}\n", .{arg});
+        var dir = try std.fs.openDirAbsolute(arg, .{ .iterate = true, .access_sub_paths = true });
+        defer dir.close();
+
+        try findFiles(allocator, &dir);
+    }
+}
 
 const std = @import("std");
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("on1namer_lib");
+const on1 = @import("on1namer_lib");
--- src/root.zig
+++ src/root.zig
@@ -5,51 +5,92 @@ const std = @import("std");
 const testing = std.testing;
 
 const FileInfo = struct {
-    name: []const u8,
+    files: []const []const u8,
     year: u16,
 
-    pub fn deinit(self: *FileInfo, allocator: std.mem.Allocator) void {
-        allocator.free(self.name);
-        self.name = "";
+    pub fn deinit(self: *FileInfo, alloc: std.mem.Allocator) void {
+        for (self.files) |file| {
+            alloc.free(file);
+        }
+        alloc.free(self.files);
+        self.files = undefined;
         self.year = 0;
     }
 };
 
-pub fn parseFile(alloc: std.mem.Allocator, path: []const u8) !FileInfo {
+const maxYear = 10000;
+
+pub fn parseFile(alloc: std.mem.Allocator, dir: *std.fs.Dir, path: []const u8) !FileInfo {
     var arena = try alloc.create(std.heap.ArenaAllocator);
     defer alloc.destroy(arena);
     arena.* = std.heap.ArenaAllocator.init(alloc);
     defer arena.deinit();
     const al = arena.allocator();
 
-    const file = try std.fs.cwd().openFile(path, .{});
+    const file = try dir.openFile(path, .{});
     defer file.close();
     const reader = file.reader();
     var jr = std.json.reader(al, reader);
     const val = try std.json.parseFromTokenSourceLeaky(std.json.Value, al, &jr, .{});
 
     const photos = val.object.get("photos").?.object;
-    if (photos.count() > 1) {
-        return error.TooManyPhotos;
+    if (photos.count() == 0) {
+        return error.NoEntry;
     }
 
+    var year: u16 = maxYear;
+    var names = std.ArrayList([]const u8).init(alloc);
+    errdefer names.deinit();
     var it = photos.iterator();
-    if (it.next()) |entry| {
+    while (it.next()) |entry| {
         const o = entry.value_ptr.*.object;
-        const name = o.get("name").?.string;
-        const captured = o.get("metadata").?.object.get("CaptureDate").?.string;
+        const name = try alloc.dupe(u8, o.get("name").?.string);
+        errdefer alloc.free(name);
+        try names.append(name);
+        if (o.get("metadata").?.object.get("CaptureDate")) |captured| {
+            const y = try std.fmt.parseInt(u16, captured.string[captured.string.len - 4 ..], 10);
+            if (y < year) {
+                year = y;
+            }
+        }
+    }
+
+    if (year < maxYear and names.items.len > 0) {
         return FileInfo{
-            .name = try alloc.dupe(u8, name),
-            .year = try std.fmt.parseInt(u16, captured[captured.len - 4 ..], 10),
+            .files = try names.toOwnedSlice(),
+            .year = year,
         };
+    } else {
+        for (names.items) |name| {
+            alloc.free(name);
+        }
+        return error.NoInfo;
     }
 
     return error.NoEntry;
 }
 
-test "parse sample" {
-    var someVal = try parseFile(std.testing.allocator, "samples/IMG_0006.on1");
+test "parse typical" {
+    var dir = std.fs.cwd();
+
+    var someVal = try parseFile(std.testing.allocator, &dir, "samples/IMG_0006.on1");
     defer someVal.deinit(std.testing.allocator);
-    try std.testing.expectEqualStrings("IMG_0006.dng", someVal.name);
+    try std.testing.expectEqual(1, someVal.files.len);
+    try std.testing.expectEqualStrings("IMG_0006.dng", someVal.files[0]);
     try std.testing.expectEqual(2025, someVal.year);
+}
+
+test "parse nodate" {
+    var dir = std.fs.cwd();
+    try std.testing.expectError(error.NoInfo, parseFile(std.testing.allocator, &dir, "samples/abyss.on1"));
+}
+
+test "parse multi-file" {
+    var dir = std.fs.cwd();
+    var someVal = try parseFile(std.testing.allocator, &dir, "samples/IMG_1648.on1");
+    defer someVal.deinit(std.testing.allocator);
+    try std.testing.expectEqual(2, someVal.files.len);
+    try std.testing.expectEqualStrings("IMG_1648.CR3", someVal.files[0]);
+    try std.testing.expectEqualStrings("IMG_1648.DNG", someVal.files[1]);
+    try std.testing.expectEqual(2022, someVal.year);
 }
